701cc02cde0e18c6e43a07058622477c837f49d5
Ethan <QustoTemmo@protonmail.com>
2024-03-02 23:06:00 +0100

    accueil, contact, à propos & plus

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-document.ready(function () {})
+document.addEventListener("DOMContentLoaded", function() {
+   const listeItems = document.querySelectorAll(".liste");
+   const place = (window.location.pathname.includes("chapitre") ? ".." : ".");
+   console.log(place)
+   let toggle = null
+   
+   listeItems.forEach(listeItem => {
+     const partie = listeItem.querySelector("ul");
+     const fléche = listeItem.querySelector("h2 img");
+     partie.classList.add("caché");
 
+     listeItem.querySelector("h2").addEventListener("click", function() {
+       if (toggle !== listeItem) {
+         listeItems.forEach(otherItem => {
+	   otherItem.querySelector("ul").classList.add("caché");
+	   otherItem.querySelector("img").setAttribute("src", `${place}/assets/off.png`)
+         });
+       }
+
+       partie.classList.toggle('caché');
+       direction = fléche.getAttribute("src") === `${place}/assets/on.png` ? `${place}/assets/off.png` : `${place}/assets/on.png`;
+       fléche.setAttribute("src", direction)
+       toggle = listeItem
+     });
+   });
+});
